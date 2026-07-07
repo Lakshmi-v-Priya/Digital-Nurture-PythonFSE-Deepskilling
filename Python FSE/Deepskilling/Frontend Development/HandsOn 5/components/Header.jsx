6ddc248fbc42f54f6cbd53d0c1000f9d342a0ef9
Lakshmi-v-Priya <151720706+Lakshmi-v-Priya@users.noreply.github.com>
2026-07-07 14:15:20 +0530

    Delete Python FSE/Deepskilling/Frontend Development/HandsOn 5 directory

--- Python FSE/Deepskilling/Frontend Development/HandsOn 5/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header>
-            <h1>Student Portal</h1>
-        </header>
-    );
-}
-
-export default Header;
--- Python FSE/Deepskilling/Frontend Development/HandsOn 5/screenshot.md
+++
@@ -1,1 +0,0 @@
-<img width="1918" height="962" alt="image" src="https://github.com/user-attachments/assets/4e227f5a-677b-4c12-8dae-56b35cd10560" />
